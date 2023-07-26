4dcfdf833f3811371571a779cc801277859e2910
yuunseo <dbstj7457@naver.com>
2023-07-27 07:33:42 +0900

    #5/2 admin web을 통한 저장 및 확인

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import RolePlayingRoom
+from .forms import RolePlayingRoomForm
 
-# Register your models here.
+
+# admin페이지에 추가될 admin 생성
+@admin.register(RolePlayingRoom)
+class RolePlayingRoomAdmin(admin.ModelAdmin):
+    # 사용할 폼 가져오기
+    form = RolePlayingRoomForm
+
+    # .user 속성 할당을 위해 메소드 재정의
+    def save_model(self, request, obj, form, change):
+        # change여부를 통해 신규 생성 여부 check 및 유효성 검사
+        if change is False and form.is_valid():
+            obj.user = request.user
+
+        super().save_model(request, obj, form, change)
