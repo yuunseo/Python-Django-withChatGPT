8a414d260d5c9c443df03190052e7feeab2970f6
yuunseo <dbstj7457@naver.com>
2023-08-01 17:52:25 +0900

    #5/1 코드  설명 추가- form와 view

--- chat/forms.py
+++ chat/forms.py
@@ -7,10 +7,11 @@ from .translators import google_translate
 class RolePlayingRoomForm(forms.ModelForm):
     class Meta:
         model = RolePlayingRoom
-        exclude = ["user"]
+        exclude = ["user"]  # user는 입력받을 수 없으며, 현재 로그인 되어있는 user가 입력돼야 함. (유효성 검사 필요)
 
     # clean method란, 폼 필드 다수에 대한 유효성 검사 수행 및
     # 값의 변환 또한 가능
+    # 한글은 존재하지만 영문은 존재하지 않는 경우, 영문 번역 후 추가
     def clean(self):
         situation = self.cleaned_data.get("situation")
         situation_en = self.cleaned_data.get("situation_en")
@@ -30,6 +31,7 @@ class RolePlayingRoomForm(forms.ModelForm):
         return self.cleaned_data
 
     # 1번째 인자: 번역할 문자열
+    # 구글 번역 기능 import해서 사용
     @staticmethod
     def _translate(origin_text: str) -> str:
         translated = google_translate(origin_text, "auto", "en")
--- chat/views.py
+++ chat/views.py
@@ -16,10 +16,13 @@ class RolePlayingRoomCreateView(CreateView):
 
     # user 데이터 전달
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
-        role_playing_room = form.dave(commit=False)
-        role_playing_room.user = self.request.user
-        return super().form_valid(form)
+        role_playing_room = form.save(
+            commit=False
+        )  # form을 통한 instance생성 시, instance.save()이 호출되지 않도록 commit=False
+        role_playing_room.user = self.request.user  # save() 전에 user field 할당
+        return super().form_valid(form)  # 이제 save() 가능 -> 부모 호출 -> save()를 통해 DB에 저장
 
 
 # .as_view()를 통해 클래스의 로직을 활용하는 새로운 뷰 함수를 생성
+# .as_view()를 통해 뷰 호출
 role_playing_room_new = RolePlayingRoomCreateView.as_view()
