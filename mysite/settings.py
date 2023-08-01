5027a345c33ab21537caf8a022e9e269a293432e
yuunseo <dbstj7457@naver.com>
2023-08-01 19:30:48 +0900

    #6/1 웹소켓을 통한 서버 준비 - ASGI

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -80,7 +81,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "mysite.wsgi.application"
-
+ASGI_APPLICATION = "mysite.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
